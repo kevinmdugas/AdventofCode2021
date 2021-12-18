976c860926aa7b0e8d9464c9de33259afa228248
kevinmdugas <77704104+kevinmdugas@users.noreply.github.com>
2021-12-17 16:55:02 -0800

    created main sub class that contains sonar and nav

---
+++ Navigation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventofCode
+{
+    internal class Navigation
+    {
+        protected int depth;
+        protected int horiz;
+        protected StreamReader sr;
+
+        public Navigation()
+        {
+            this.depth = 0;
+            this.horiz = 0;
+
+            try
+            {
+                string file = "../../../NavigationInput.txt";
+                this.sr = new StreamReader(file);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The navigation file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+        
+        public int ReadCourse()
+        {
+            string line;
+            string[] substrings;
+            while((line = this.sr.ReadLine()) != null)
+            {
+                substrings = line.Split(' ');
+                foreach (var substring in substrings)
+                    Console.WriteLine(substring);
+            }
+            return 0;
+        }
+        
+    }
+    
+
+}
---
+++ Submarine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventofCode
+{
+    internal class Submarine
+    {
+        protected SonarSweep sonar;
+        protected Navigation nav;
+
+        public Submarine()
+        {
+            this.sonar = new SonarSweep();
+            this.nav = new Navigation();
+        }
+
+        static void Main(string[] args)
+        {
+            Submarine sub = new Submarine();
+            sub.sonar.TopMenu();
+        }
+    }
+}
